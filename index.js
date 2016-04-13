3b36e793d799101b5433cd1e569bbeba84546151
Ross Brown <ross@cremalab.com>
2016-04-13 17:14:00 -0500

    adding accelerometer input source

--- index.js
+++ index.js
@@ -1,21 +1,33 @@
 'use strict'
 
-const tessel       = require('tessel')
-const synthax      = require('synthax')
-const AmbientLight = require('./lib/input_sources/Ambient').Light
-const AmbientSound = require('./lib/input_sources/Ambient').Sound
+const tessel        = require('tessel')
+const synthax       = require('synthax')
+const AmbientLight  = require('./lib/input_sources/Ambient').Light
+const AmbientSound  = require('./lib/input_sources/Ambient').Sound
+const Accelerometer = require('./lib/input_sources/Accelerometer')
 
-const light = new AmbientLight()
-const sound = new AmbientSound()
+// const light = new AmbientLight()
+// const sound = new AmbientSound()
+const accelZ = new Accelerometer.Z(1000)
+const accelX = new Accelerometer.X()
 
-// 
+//
 // sound.connect()
 // sound.output.on('signal', (x) => {
 //   console.log(`sound: ${x}`);
 // })
 //
 
-light.connect()
-light.output.on('signal', (x) => {
-  console.log(`light: ${x}`);
+// light.connect()
+// light.output.on('signal', (x) => {
+//   console.log(`light: ${x}`);
+// })
+accelZ.connect()
+accelZ.output.on('signal', (x) => {
+  console.log(`accel Z: ${x}`);
+})
+
+accelX.connect()
+accelX.output.on('signal', (x) => {
+  console.log(`accel X: ${x}`);
 })
