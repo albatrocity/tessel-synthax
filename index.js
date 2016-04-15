0452a6a4fb73e1329efaebfe7ed2c0edf5227f62
Ross Brown <ross@cremalab.com>
2016-04-15 10:41:27 -0500

    adding Microphone input_source

--- index.js
+++ index.js
@@ -6,16 +6,24 @@ const AmbientLight  = require('./lib/input_sources/Ambient').Light
 const AmbientSound  = require('./lib/input_sources/Ambient').Sound
 const Accelerometer = require('./lib/input_sources/Accelerometer')
 const VideoCamera   = require('./lib/input_sources/Camera').Video
+const Microphone    = require('./lib/input_sources/Microphone')
+const av            = require('tessel-av')
 
 const light = new AmbientLight()
-// const sound = new AmbientSound()
+const sound = new AmbientSound()
 const accelZ = new Accelerometer.Z(1000)
 const accelX = new Accelerometer.X()
-console.log('start!');
 const video  = new VideoCamera()
 video.connect()
 video.output.on('signal', (x) => {
   console.log('cameraData')
+  // console.log(x);
+})
+
+const mic  = new Microphone()
+mic.connect()
+mic.output.on('signal', (x) => {
+  console.log('micData')
   // console.log(x);
 })
 
@@ -26,16 +34,16 @@ video.output.on('signal', (x) => {
 // })
 //
 
-// light.connect()
-// light.output.on('signal', (x) => {
-//   console.log(`light: ${x}`);
-// })
-// accelZ.connect()
-// accelZ.output.on('signal', (x) => {
-//   console.log(`accel Z: ${x}`);
-// })
-//
-// accelX.connect()
-// accelX.output.on('signal', (x) => {
-//   console.log(`accel X: ${x}`);
-// })
+light.connect()
+light.output.on('signal', (x) => {
+  console.log(`light: ${x}`);
+})
+accelZ.connect()
+accelZ.output.on('signal', (x) => {
+  console.log(`accel Z: ${x}`);
+})
+
+accelX.connect()
+accelX.output.on('signal', (x) => {
+  console.log(`accel X: ${x}`);
+})
